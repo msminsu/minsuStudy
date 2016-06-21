15b49fb251b147464344c1f480a6f5a8208ebd4a
msminsu <msminsu@nate.com>
2016-06-21 15:48:40 +0900

    up load

--- scriptStudy/studying.js
+++ scriptStudy/studying.js
@@ -1,15 +1,37 @@
+// 객체 생성하는 함수 : 생성자 함수 (Constructor Function)
 
-var myObj = (function(){
-    // 비공개 멤버
-    var name = 'my oh my';
+function Navigation(el){
+    this.el = document.querySelector(el);
+   // this.children = this.el.querySelectorAll('a');
+   // this.childrenLength = this.children.length;
+}
 
-    //공개될 부분을 구현한다.
-    return {
-        // 특권 메서드
-        getName : function(){
-            return name;
-        }
+//생성자 함수의 프로토 타입: 생성되는 객체의 원형(Prototype)
+
+Navigation.prototype = {
+    nextLink : function () {
+        console.log(this.el, '다음 링크 활성화');
+    },
+    prevLink: function(){
+        console.log(this.el, '이전 링크 활성화');
+    },
+    goToLink : function(num){
+        console.log(num + '링크 활성화');
+    },
+    playRollingLinks :function () {
+        console.log('링크 롤링 시작');
+    },
+    stopRollingLinks: function () {
+        console.log('링크 롤링 중지');
     }
-}());
+};
 
-console.log(myObj.getName());
+// Navigation 인스턴스 객체 생성
+var hNav = new Navigation('header nav');
+var aNav = new Navigation('aside nav');
+var mNav = new Navigation('main nav');
+var fNav = new Navigation('footer nav');
+
+// Navigation 인스턴스 객체 메소드 활용
+hNav.nextLink();
+mNav.prevLink();
--- test.js
+++ test.js
@@ -1,11 +1,1 @@
-/**
- * Created by msminsu on 2016-05-16.
- */
-
-function Door(num) {
-    this.name = num;
-}
-
-
-var door = new Door('green');
-
+/**k
