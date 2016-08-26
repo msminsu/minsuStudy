a8a5acf7c389f838f11dd5bda8b3b500f100d365
msminsu <msminsu@nate.com>
2016-08-26 09:27:23 +0900

    up load

--- scriptStudy/studying.js
+++ scriptStudy/studying.js
@@ -1,37 +1,21 @@
-// 객체 생성하는 함수 : 생성자 함수 (Constructor Function)
 
-function Navigation(el){
-    this.el = document.querySelector(el);
-   // this.children = this.el.querySelectorAll('a');
-   // this.childrenLength = this.children.length;
+/*
+*
+* 사용된 구분자 = ","
+* "minsu,1234"
+*
+* 사용된 구분자 = "||"
+* "id=minsu||pw=1234"
+*
+* 사용된 구분자  = "&"
+* "id=minsu&pw=1234"
+* */
+
+//클라이언트는 서버에서 사용한 구분자를 토대로 결과값을 분리해서 데이터를 처리
+var aryData = xmlHttp.responseText.splite('||');
+var objResult = {};
+for( var i = 0; i< aryData.length; i++){
+    var keyValue = aryData[i].splite('=');
+    objResult[keyValue[0]] = keyValue[1];
 }
 
-//생성자 함수의 프로토 타입: 생성되는 객체의 원형(Prototype)
-
-Navigation.prototype = {
-    nextLink : function () {
-        console.log(this.el, '다음 링크 활성화');
-    },
-    prevLink: function(){
-        console.log(this.el, '이전 링크 활성화');
-    },
-    goToLink : function(num){
-        console.log(num + '링크 활성화');
-    },
-    playRollingLinks :function () {
-        console.log('링크 롤링 시작');
-    },
-    stopRollingLinks: function () {
-        console.log('링크 롤링 중지');
-    }
-};
-
-// Navigation 인스턴스 객체 생성
-var hNav = new Navigation('header nav');
-var aNav = new Navigation('aside nav');
-var mNav = new Navigation('main nav');
-var fNav = new Navigation('footer nav');
-
-// Navigation 인스턴스 객체 메소드 활용
-hNav.nextLink();
-mNav.prevLink();
